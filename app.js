342b187fa953c4c0fe086627545fec3db819d4a3
freddyline Barreto <documentosfbq@hotmail.com>
2025-09-21 22:42:31 -0300

    modificacion de linea2 por error.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//vaiable
 let amigos = [];
 //agregar amigos
 function agregarAmigo(){
